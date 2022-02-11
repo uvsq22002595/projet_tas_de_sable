ab2e7890200e0908ae14b5abd4a6014070f638e0
Iduwara WICKRAMASINGHE <iduwara.wickramasinghe@ens.uvsq.fr>
2022-02-11 15:18:29 +0100

    debut de projet

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -1,1 +1,19 @@
-projet
+#########################################
+# groupe MI TD03
+# Bertrand Noah 
+# Wickramasinghe Adipthya Iduwara
+# https://github.com/uvsq-info/l1-python
+#########################################
+
+import tkinter as tk
+
+HAUTEUR = 600
+LARGEUR = 600
+
+racine = tk.Tk()
+racine.title("Tas de sables")
+canvas = tk.Canvas(racine,height=HAUTEUR, width=LARGEUR)
+canvas.grid(column=1,row=0)
+
+
+racine.mainloop()
