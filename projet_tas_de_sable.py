aa5d023c85141101ca7ce3d007e5175a8876a568
Noah BERTRAND <noah.bertrand@ens.uvsq.fr>
2022-02-15 14:37:51 +0100

    résolution du cadrillage

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -44,12 +44,43 @@ def affichage(CADRE):
 def valeur(CADRE):
     """Cette fonction permet de récupérer uniquement les valeurs de la grille"""
     #print("le cardre", cadre)
-    liste_valeur = [[], [], []]
+    LISTE_VALEUR = []
     for a in range(len(CADRE)): 
         for b in range (len(CADRE)):
             if type(CADRE[a][b]) == int :
-                liste_valeur[a-1].append(CADRE[a][b])
-    return liste_valeur
+                LISTE_VALEUR.append(CADRE[a][b])
+    return LISTE_VALEUR
+
+def avalanche(CADRE):
+    print("init\n" + affichage(CADRE))
+    test = True
+    result = CADRE
+    while test == True:
+        for a in range(len(CADRE)):
+            for b in range(len(CADRE[a])):
+                if type(CADRE[a][b]) == int and CADRE[a][b]>3:
+                    result[a][b] -= 4
+                    if CADRE[a-1][b] != "#":
+                        result[a-1][b] += 1
+                    if CADRE[a+1][b] != "#":
+                        result[a+1][b] += 1
+                    if CADRE[a][b-1] != "#":
+                        result[a][b-1] += 1
+                    if CADRE[a][b+1] != "#":
+                        result[a][b+1] += 1
+        print(affichage(result))
+        cadre = result
+        n = valeur(CADRE)
+        test = False
+        for c in range(4,10):
+            if c in n:
+                test = True
+        print(test)
+    return result
+
+    
+
+
 
 
 
@@ -57,9 +88,10 @@ def valeur(CADRE):
 #######################################
 # appel à fonction
 
-A = creation()
-print(affichage(A))
-print(valeur(A))
+creation()
+print(affichage(creation()))
+print(valeur(creation()))
+print(avalanche(creation()))
 #######################################
 # ajout du compteur
 #label_counter = Label(racine, text="5", font=("Courrier", 30), bg="#dee5dc")
