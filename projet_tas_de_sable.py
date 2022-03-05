a9d97a1ca63ac98f7d9e1d1867c2c0bfe44b033d
Noah BERTRAND <noah.bertrand@ens.uvsq.fr>
2022-03-05 12:36:38 +0100

    Programme fonctionel, qui résout presque tout ce qui étais demandé normalement

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -7,6 +7,7 @@
 
 import tkinter as tk
 from random import *
+from time import *
 
 racine = tk.Tk()
 racine.title("Tas de sables")
@@ -57,42 +58,7 @@ def valeur(CADRE):
                 LISTE_VALEUR[a].append(CADRE[a][b])
     LISTE_VALEUR.remove(LISTE_VALEUR[0])
     LISTE_VALEUR.remove(LISTE_VALEUR[-1])
-    """for a in CADRE: 
-        for b in a:
-            if type(b) == int :
-                LISTE_VALEUR.append(b)"""
     return LISTE_VALEUR
-
-"""def avalanche(LISTE):
-    Cette fonction permet de déclancher l'avalanche et donc de faire tourner le programme
-    CADRE = LISTE[0]
-    print(LISTE)
-    print("init\n" + affichage(CADRE))
-    test = True
-    result = CADRE
-    while test == True:
-        for a in range(LISTE[2]):
-            for b in range(LISTE[1]):
-                if type(CADRE[a][b]) == int and CADRE[a][b]>3:
-                    result[a][b] -= 4
-                    if CADRE[a-1][b] != "#":
-                        result[a-1][b] += 1
-                    if CADRE[a+1][b] != "#":
-                        result[a+1][b] += 1
-                    if CADRE[a][b-1] != "#":
-                        result[a][b-1] += 1
-                    if CADRE[a][b+1] != "#":
-                        result[a][b+1] += 1
-        print( "affichage : ", affichage(result))
-        cadre = result
-        n = valeur(CADRE)
-        test = False
-        for c in range(4, 10):
-            for d in n:
-                for e in d:
-                    if e == c:
-                        test = True
-    return result"""
 
 def avalanche(LISTE):
     """Cette fonction permet de déclancher l'avalanche et donc de faire tourner le programme"""
@@ -128,6 +94,38 @@ def avalanche(LISTE):
     #print("dernier result : ", result)
     return result
 
+def avalanche_unique(LISTE):
+    """Cette fonction permet de déclancher l'avalanche et donc de faire tourner le programme"""
+    CADRE = LISTE
+    print("init\n" + affichage(CADRE))
+    test = True
+    result = CADRE
+    while test == True:
+        for a in range(len(CADRE)):
+            for b in range(len(CADRE)):
+                #print( "cardre_a_B : ",CADRE[a][b])
+                if type(CADRE[a][b]) == int and CADRE[a][b]>3:
+                    result[a][b] -= 4
+                    if CADRE[a-1][b] != "#":
+                        result[a-1][b] += 1
+                    if CADRE[a+1][b] != "#":
+                        result[a+1][b] += 1
+                    if CADRE[a][b-1] != "#":
+                        result[a][b-1] += 1
+                    if CADRE[a][b+1] != "#":
+                        result[a][b+1] += 1
+        return result
+    """   #print( result : , result)
+        cadre = result
+        n = valeur(CADRE)
+        test = False
+        for c in range(4, 10):
+            for d in n:
+                for e in d:
+                    if e == c:
+                        test = True
+    #print("dernier result : ", result)
+    return result"""
 #######################################
 # appel à fonction
 #print(creation(n))
@@ -160,18 +158,44 @@ def affichage_valeurs(CADRE, n):
     emplacement_y = hauteur_case // 2
     for a in CADRE:
         for b in a:
-            canvas.create_text(emplacement_x, emplacement_y, fill = "red", text = str(b))
+            canvas.create_text(emplacement_x, emplacement_y, fill = "red", text = str(b), font=("Purisa", 150//n))
             emplacement_x += largeur_case 
         emplacement_x = largeur_case // 2
         emplacement_y += hauteur_case 
 
-def avalanche_graphique(CADRE):
-    """Cette fonction permet d'effectuer toutes les avalanches """
-    New_CADRE = avalanche(CADRE)
+def toutes_avalanches_graphique(CADRE):
+    """Cette fonction permet d'effectuer toutes les avalanches"""
+
+
+    New_CADRE = avalanche_unique(CADRE)
     grillage(n)
     affichage_valeurs(valeur(New_CADRE), n)
+    for a in range(len(CADRE)):
+        for b in range(len(CADRE)):
+            if New_CADRE[a][b] != " " and New_CADRE[a][b] != "#" and New_CADRE[a][b] > 3 :
+                print("valeur cadre AB", New_CADRE[a][b])
+                #sleep(0.75)
+                toutes_avalanches_graphique(New_CADRE)
+    
+
+    """for c in range(4, 10):
+            for d in CADRE:
+                for e in d:
+                    if e == c:
+                        New_CADRE = avalanche_unique(CADRE)
+                        print(New_CADRE)
+                        grillage(n)
+                        affichage_valeurs(valeur(avalanche_unique(CADRE)), n)
+                        #sleep(0.75)
+                        toutes_avalanches_graphique(New_CADRE)"""
+                
 
     
+
+def avalanche_unique_graphique(CADRE):
+    New_CADRE = avalanche_unique(CADRE)
+    grillage(n)
+    affichage_valeurs(valeur(New_CADRE), n)
 
 ##############################@
 #variable partie graphique
@@ -193,7 +217,9 @@ bouton_cadre.grid(column = 1, row = 2)
 bouton_affichage = tk.Button(racine, text = "Affichage des valeurs", fg = "black", command = lambda : affichage_valeurs(CADRE,n), activebackground = "blue", borderwidth=2, bg = "green")
 bouton_affichage.grid(column = 0, row = 1)
 
-bouton_avalanche = tk.Button(racine, text = "déclencher une avalenche", fg = "black", command = lambda : avalanche_graphique(CADRE_0), activebackground = "blue", borderwidth=2, bg = "green")
-bouton_avalanche.grid(column = 0, row = 2)
+bouton_avalanche = tk.Button(racine, text = "Effectuer toute les avalanches", fg = "black", command = lambda : toutes_avalanches_graphique(CADRE_0), activebackground = "blue", borderwidth=2, bg = "green")
+bouton_avalanche.grid(column = 0, row = 3)
 
+bouton_avalanche_unique = tk.Button(racine, text = "Déclencher une avalanche", fg = "black", command = lambda : avalanche_unique_graphique(CADRE_0), activebackground = "blue", borderwidth=2, bg = "green")
+bouton_avalanche_unique.grid(column = 0, row = 2)
 racine.mainloop()
